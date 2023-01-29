103edf8387ad0330f03fe17aa1c2abe599d11550
Sergei Morozov <morozov@tut.by>
2023-01-28 19:25:00 -0800

    More details on lastInsertId()

--- bin/dbal.php
+++ bin/dbal.php
@@ -23,12 +23,31 @@ function connect_to_oci8() {
     ]);
 }
 
+function connect_to_sqlsrv() {
+    return DriverManager::getConnection([
+        'driver'   => 'sqlsrv',
+        'host'     => '127.0.0.1',
+        'user'     => 'sa',
+        'password' => 'Passw0rd',
+    ]);
+}
+
 function connect_to_pdo_mysql() {
     return DriverManager::getConnection([
         'driver' => 'pdo_mysql',
         'host'   => '127.0.0.1',
         'user'   => 'root',
         'dbname' => 'mysql',
+    ]);
+}
+
+function connect_to_pdo_oci() {
+    return DriverManager::getConnection([
+        'driver'   => 'pdo_oci',
+        'host'     => '127.0.0.1',
+        'user'     => 'system',
+        'password' => 'oracle',
+        'dbname'   => 'XE',
     ]);
 }
 
@@ -50,9 +69,13 @@ function connect_to_pdo_sqlite() {
 
 function connect_to_pdo_sqlsrv() {
     return DriverManager::getConnection([
-        'driver'   => 'sqlsrv',
+        'driver'   => 'pdo_sqlsrv',
         'host'     => '127.0.0.1',
         'user'     => 'sa',
         'password' => 'Passw0rd',
     ]);
+}
+
+function connect() {
+    return connect_to_pdo_oci();
 }
